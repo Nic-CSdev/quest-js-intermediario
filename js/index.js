65f5f286b2bf011985e0be17d04e5629edf0d493
Nicolas <127686097+Nic-CSdev@users.noreply.github.com>
2024-02-12 23:23:56 -0300

    melhorando a forma de fazer com que o formulário não seja enviado

--- js/index.js
+++ js/index.js
@@ -1,27 +1,25 @@
-const botao = document.querySelector('#btn-enviar')
-const campos = document.querySelectorAll('.campo')
-const avisoCampoObrigatorio = document.querySelectorAll('.aviso')
+const botao = document.querySelector("#btn-enviar");
+const campos = document.querySelectorAll(".campo");
+const avisoCampoObrigatorio = document.querySelectorAll(".aviso");
 
-botao.addEventListener('click', () => {
-    campos.forEach((campo, indice) => {
-        if(campo.value === ''){
-            campo.classList.add('campo-vazio')
-            avisoCampoObrigatorio[indice].innerHTML = "<p>campo obrigatório</p>"
-        }
-        else{
-            campo.classList.remove('campo-vazio')
-            avisoCampoObrigatorio[indice].innerHTML = ""
-        }
-    })
-})
+botao.addEventListener("click", () => {
+  campos.forEach((campo, indice) => {
+    if (campo.value === "") {
+      campo.classList.add("campo-vazio");
+      avisoCampoObrigatorio[indice].innerHTML = "<p>campo obrigatório</p>";
+    } else {
+      campo.classList.remove("campo-vazio");
+      avisoCampoObrigatorio[indice].innerHTML = "";
+    }
+  });
+});
 
-campos.forEach(campo => {
-    campo.addEventListener('change', () => {
-        if(campo.value !== ""){
-            campo.classList.add('campo-preenchido')
-        }
-        else{
-            campo.classList.remove('campo-preenchido')
-        }
-    })
-})
+campos.forEach((campo) => {
+  campo.addEventListener("change", () => {
+    if (campo.value !== "") {
+      campo.classList.add("campo-preenchido");
+    } else {
+      campo.classList.remove("campo-preenchido");
+    } 
+  });
+});
